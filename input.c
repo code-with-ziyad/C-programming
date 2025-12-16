5ba82816c210891dd0f16965b626451f57805c61
ZIYAD AFTAB <ziyadyt94@gmail.com>
2025-12-16 18:41:42 +0500

    Add files via upload

---
+++ input.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	int a, b;
+	
+	 printf("Enter a first number: ");
+	 scanf("%d" , &a);
+	 printf("Enter a second number: ");
+	 scanf("%d" , &b);
+	 
+	 int sum = a + b;
+	 printf("The sam of two numbers %d", sum);
+
+}
