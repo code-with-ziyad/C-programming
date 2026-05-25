502b98860314a938f79981d2397eb0c9edcefe78
ZIYAD AFTAB <ziyadyt94@gmail.com>
2026-05-24 22:19:35 -0700

    Add files via upload

---
+++ operators.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+int main()
+{
+    // Operators
+
+    // Arithmetic
+    int a = 5, b = 6, c = 7;
+
+    printf("The sum of  a and b %d\n", a + b);
+    printf("The multiplication of  a and b %d\n", a * b);
+    printf("The division of  a and b %d\n", a / b);
+    printf("The a minus b %d\n", a - b);
+    printf("The reminder of a and b %d\n", a % b);
+
+    // incrament and decreament
+
+    printf("%d\n", ++c);
+    printf("%d\n", --c);
+
+    // logical Operators 
+
+     /*
+     AND = &&
+     OR  = ||
+     NOT = !
+     */
+
+    // Assignment Operators
+
+/*
+          =             a = 5;
+          +=            a+ = 2
+          -=            a- = 2
+          /=            a/ = 2
+          %=            a% = 2
+*/
+
