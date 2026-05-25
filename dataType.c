deabddbb10cede0948d620f1da99b25cf59fee55
ZIYAD AFTAB <ziyadyt94@gmail.com>
2026-05-24 21:43:23 -0700

    Add files via upload

---
+++ dataType.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+
+   // int declaration
+    int a = 8;  
+    printf("%d\n", a);
+
+    //float variable declaration
+    float b = 4.0;
+    printf("%f\n", b);
+
+    // char declaration
+    char c = 'Z';
+    printf("%c", c);
+
+    
+}
